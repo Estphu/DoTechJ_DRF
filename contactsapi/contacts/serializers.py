cfe9f5b7b3f83562bbe79b91f9cebc435e408efd
Estphu <jaydee20001217@gmail.com>
2023-10-03 23:37:50 +0500

    contacts API folder Deleted

--- contactsapi/contacts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Contact
-
-class ContactSerializer(serializers.ModelSerializer):
-
-    class Meta():
-        model = Contact
-        fields = ['country_code','owner','phone_number','first_name','last_name','contact_picture','is_favourite']
--- contactsapi/contacts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.ContactAPIList.as_view()),
-    path('<int:pk>',views.ContactAPIDetail.as_view())
-]
