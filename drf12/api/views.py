24ba14680a2d552f40a91c762862a7f203f4e081
Estphu <jaydee20001217@gmail.com>
2023-10-06 15:51:32 +0500

    added ModelValueSet

---
+++ drf12/api/views.py
@@ -0,0 +1,15 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+
+class StudentMVS(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentROMVS(viewsets.ReadOnlyModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+             
+
