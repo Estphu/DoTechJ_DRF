c4e8d02a330beab1d70e2fb33fe3e156c71bd1f3
Estphu <jaydee20001217@gmail.com>
2023-10-07 13:57:02 +0500

    added default (authentication & permissions) in settings.py

--- drf13/drf13/settings.py
+++ drf13/drf13/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ['resrt_framework.authentication.BasicAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
