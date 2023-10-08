3773851eb761b65b9e16a575e5b0bd55bcbd9732
Estphu <jaydee20001217@gmail.com>
2023-10-08 13:38:02 +0500

    updated drf7 (added authentication and permission using decorators)

--- drf7/api/views.py
+++ drf7/api/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 @api_view(['GET','POST','PUT', 'PATCH','DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def student_api(request,pk=None):
     if request.method == "GET":
         if pk is not None:
