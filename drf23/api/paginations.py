2ba9a185b276d10c33c194442978bbba42a278b8
Estphu <jaydee20001217@gmail.com>
2023-10-25 15:46:09 +0500

    added Pagination-LimitOffsetPagination in drf23

--- drf23/api/paginations.py
+++ drf23/api/paginations.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
 class MyPageNumberPagination(PageNumberPagination):
     page_size = 5
@@ -6,3 +7,9 @@ class MyPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'records'
     max_page_size = 7
     last_page_strings = 'end'
+
+class MyLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 5
+    limit_query_param = 'mylimit'
+    offset_query_param = 'myoffset'
+    max_limit = 7
--- drf23/api/views.py
+++ drf23/api/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
-from .paginations import MyPageNumberPagination
+from .paginations import MyPageNumberPagination, MyLimitOffsetPagination
 from rest_framework.generics import ListAPIView
 
 
 # Create your views here.
-class StudentAPIList(ListAPIView, MyPageNumberPagination):
+class StudentAPIList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    pagination_class = MyPageNumberPagination
+    pagination_class = MyLimitOffsetPagination
 
 
