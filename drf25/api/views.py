e63a0a798cd50ffb7def2ff7509082e9cda7d563
Estphu <jaydee20001217@gmail.com>
2023-10-26 14:47:19 +0500

    added HyperLinkedModelSerializer in drf25

---
+++ drf25/api/views.py
@@ -0,0 +1,11 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+
+class StudentMVS(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+             
+
